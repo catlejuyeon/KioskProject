e8ebe0770ce34ce3e4d6aacf6610fde3bb18df55
castlejuyeon <jucess1106@naver.com>
2025-10-26 16:38:53 +0900

    Refactor: Discount 이넘 클래스 할인 메서드 수정

---
+++ src/com/example/kiosk/Discount.java
@@ -0,0 +1,32 @@
+package com.example.kiosk;
+
+public enum Discount {
+    NATIONALMERIT("국가유공자",0.1),
+    MILITARY("군인",0.05),
+    STUDENT("학생",0.03),
+    PERSON("일반인",1);
+
+    private final double rate;
+    private final String discountName;
+
+    Discount(String discountName, double rate) {
+        this.rate = rate;
+        this.discountName=discountName;
+    }
+
+    public int getDiscount(int price) {
+        return (int)(price * rate);
+    }
+
+    public int getFinalPrice(int price) {
+        return price - getDiscount(price);
+    }
+
+    public int apply(int price){
+        return (int) (price*rate);
+    }
+
+    public String getDiscountName(){
+        return discountName;
+    }
+}
